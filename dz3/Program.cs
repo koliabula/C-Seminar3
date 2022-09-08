aa08a9956f92c68a7d2333135e1d89d57c822521
Bulla Kolia <koliakolan@gmail.com>
2022-09-08 11:29:53 +0300

    DZ

---
+++ dz3/Program.cs
@@ -0,0 +1,111 @@
+﻿/*Задача 19  Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+Console.Clear();
+int i = 0;
+int n = 0;
+Console.WriteLine("Введите номер задачи:");
+while (i == 0){
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n > 0 & n < 4){
+        break;
+    }
+    else {
+        Console.WriteLine("Введите номер задачи, которую хотите решить:");
+        Console.WriteLine("1 - проверка на полиндром:");
+        Console.WriteLine("2 - расстояние между двумя точувми в 3D пространстве:");
+        Console.WriteLine("3 - Таблица кубов от 1 до N:");
+
+    }
+}
+if (n == 1){    //Задача 19  Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.  
+
+    void Polindron(int num){
+        if((num / 10000 == num % 10) & ((num / 1000) % 10 == (num / 10) % 10) ){
+            Console.WriteLine($"Число {num} --- Полиндром!!!");
+        }
+        else {
+            Console.WriteLine($"Число {num} --- НЕ Полиндром!!!");
+        }
+    }
+            
+    while(i == 0){   
+        Console.WriteLine("Введите пятизначное число");                           
+        int number = Convert.ToInt32(Console.ReadLine());                                
+        if (number > 9999 & number < 100000) {                               
+            Polindron(number);
+        }
+        else {
+            Console.WriteLine("Ошибка!!! Вы ввели не пятизначное число!!! "); 
+        }
+        Console.WriteLine("Хотите попробывать ещё раз???");
+        Console.WriteLine("Введите 'да' или 'нет'");
+        string test = Console.ReadLine();
+        if(test.ToLower() == "да"){}
+        else {break;}
+    }
+}
+
+else if (n == 2){    //Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+    double Rasst(double x1, double y1, double z1, double x2, double y2, double z2 )
+        {
+            double rasst = Math.Sqrt(Math.Pow( x2 - x1, 2)+Math.Pow(y2 - y1, 2)+Math.Pow(z2 - z1, 2));
+            Console.WriteLine("rasst:" + rasst);
+            return rasst;
+        }
+
+    while(i == 0){
+        
+        Console.WriteLine("Введите x1:");
+        double x1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите y1:");
+        double y1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите z1:");
+        double z1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите x2:");
+        double x2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите y2:");
+        double y2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите z2:");
+        double z2 = Convert.ToDouble(Console.ReadLine());
+
+        Rasst(x1, y1, z1, x2, y2, z2);
+
+        Console.WriteLine("Хотите попробывать ещё раз???");
+        Console.WriteLine("Введите 'да' или 'нет'");
+        string test = Console.ReadLine();
+        if(test.ToLower() == "да"){}
+        else {break;}
+    }
+}
+
+else if(n == 3){//Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+    
+    void PowPowPow(int n){
+            for(int i = 1; i <= n; i++){
+            Console.WriteLine(Math.Pow(i,3));
+            }
+        }
+    
+    while(i == 0){
+
+        Console.WriteLine("Введите Число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        PowPowPow(number);
+
+        Console.WriteLine("Хотите попробывать ещё раз???");
+        Console.WriteLine("Введите 'да' или 'нет'");
+        string test = Console.ReadLine();
+        if(test.ToLower() == "да"){}
+        else {break;}
+    }
+}
--- work1/Program.cs
+++ work1/Program.cs
@@ -1,106 +1,108 @@
 ﻿/*17. Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
 Задача */
 
-Console.Clear();
-int i = 0;
-int n = 0;
-Console.WriteLine("Введите номер задачи:");
-while (i == 0){
-    n = Convert.ToInt32(Console.ReadLine());
-    if (n > 0 & n < 5){
-        break;
-    }
-    else {
-        Console.WriteLine("Введите номер задачи от 1 до 3:");
-    }
-}
-if (n == 1){
-    Console.WriteLine("Введите x:");
-    int x = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Введите y:");
-    int y = Convert.ToInt32(Console.ReadLine());
-    
 
-    if (x == 0 || y == 0){Console.WriteLine("x и y не должны быть равны 0");}
+    Console.Clear();
+    int i = 0;
+    int n = 0;
+    Console.WriteLine("Введите номер задачи:");
+    while (i == 0){
+        n = Convert.ToInt32(Console.ReadLine());
+        if (n > 0 & n < 5){
+            break;
+        }
+        else {
+            Console.WriteLine("Введите номер задачи от 1 до 3:");
+        }
+    }
+    if (n == 1){
+        Console.WriteLine("Введите x:");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите y:");
+        int y = Convert.ToInt32(Console.ReadLine());
+        
 
-    else{
-        if(x > 0 & y > 0)
+        if (x == 0 || y == 0){
+            Console.WriteLine("x и y не должны быть равны 0");
+        }
+
+        else{
+            if(x > 0 & y > 0)
+            {
+                Console.WriteLine("1 Четверть");
+            }
+            if(x < 0 & y > 0)
+            {
+                Console.WriteLine("2 Четверть");
+            }
+            if(x < 0 & y < 0)
+            {
+                Console.WriteLine("3 Четверть");
+            }
+            if(x > 0 & y < 0)
+            {
+                Console.WriteLine("4 Четверть");
+            }
+        }
+    }
+
+    else if (n == 2){  //18: Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+        Console.WriteLine("Введите номер четверти:");
+        int chet = Convert.ToInt32(Console.ReadLine());
+        if(chet > 0 & chet < 5){
+            if(chet == 1){
+                Console.WriteLine("x > 0; y > 0");
+            }
+            else if(chet == 2){
+                Console.WriteLine("x < 0; y > 0");
+            }
+            else if(chet == 3){
+                Console.WriteLine("x < 0; y < 0");
+            }
+            else if(chet == 4){
+                Console.WriteLine("x > 0; y < 0");
+            }
+        }
+        else {
+            Console.WriteLine("Всего четыре четверти ");
+        }
+    }
+
+
+    else if (n == 3){/*Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+    A (3,6); B (2,1) -> 5,09 
+    A (7,-5); B (1,-1) -> 7,21*/
+        double Rasst(double x1, double y1, double x2, double y2)
         {
-            Console.WriteLine("1 Четверть");
+            double rasst = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));
+            Console.WriteLine("rasst:" + rasst);
+            return rasst;
         }
-        if(x < 0 & y > 0)
-        {
-            Console.WriteLine("2 Четверть");
-        }
-        if(x < 0 & y < 0)
-        {
-            Console.WriteLine("3 Четверть");
-        }
-        if(x > 0 & y < 0)
-        {
-            Console.WriteLine("4 Четверть");
-        }
-    }
-}
 
-if (n == 2){  //18: Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
-    Console.WriteLine("Введите номер четверти:");
-    int chet = Convert.ToInt32(Console.ReadLine());
-    if(chet > 0 & chet < 5){
-        if(chet == 1){
-            Console.WriteLine("x > 0; y > 0");
-        }
-        if(chet == 2){
-            Console.WriteLine("x < 0; y > 0");
-        }
-        if(chet == 3){
-            Console.WriteLine("x < 0; y < 0");
-        }
-        if(chet == 4){
-            Console.WriteLine("x > 0; y < 0");
-        }
-    }
-    else {
-        Console.WriteLine("Всего четыре четверти ");
-    }
-}
+        Console.WriteLine("Введите x1:");
+        double x1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите y1:");
+        double y1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите x2:");
+        double x2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите y2:");
+        double y2 = Convert.ToDouble(Console.ReadLine());
 
-
-if (n == 3){
-    /*Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
-A (3,6); B (2,1) -> 5,09 
-A (7,-5); B (1,-1) -> 7,21*/
-    double Rasst(double x1, double y1, double x2, double y2)
-    {
-        double rasst = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));
-        Console.WriteLine("rasst:" + rasst);
-        return rasst;
+        Rasst(x1, y1, x2, y2);
     }
 
-    Console.WriteLine("Введите x1:");
-    double x1 = Convert.ToDouble(Console.ReadLine());
-    Console.WriteLine("Введите y1:");
-    double y1 = Convert.ToDouble(Console.ReadLine());
-    Console.WriteLine("Введите x2:");
-    double x2 = Convert.ToDouble(Console.ReadLine());
-    Console.WriteLine("Введите y2:");
-    double y2 = Convert.ToDouble(Console.ReadLine());
+    else if (n == 4){ ///Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+        //5 -> 1, 4, 9, 16, 25.
+        //2 -> 1,4
 
-    Rasst(x1, y1, x2, y2);
-}
-
-if (n == 4){ ///Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
-    //5 -> 1, 4, 9, 16, 25.
-    //2 -> 1,4
-
-    void PowPow(int n){
-        for(int i = 1; i <= n; i++){
-        Console.WriteLine(Math.Pow(i,2));
+        void PowPow(int n){
+            for(int i = 1; i <= n; i++){
+            Console.WriteLine(Math.Pow(i,2));
+            }
         }
+        
+        Console.WriteLine("Введите Число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        PowPow(number);
     }
-    
-    Console.WriteLine("Введите Число:");
-    int number = Convert.ToInt32(Console.ReadLine());
-    PowPow(number);
-}
 
